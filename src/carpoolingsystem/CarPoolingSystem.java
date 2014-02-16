2c7b3168a32b693ce5350321d8ea02454cce5535
AnnamalaiAnna <annamalai1.a@gmail.com>
2014-02-16 16:53:26 -0500

    Day 1 Changes made by Annamalai, Rajesh

--- src/carpoolingsystem/CarPoolingSystem.java
+++ src/carpoolingsystem/CarPoolingSystem.java
@@ -3,43 +3,85 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package carpoolingsystem;
 
 import java.util.Date;
+import java.util.LinkedList;
 import org.jdesktop.swingx.JXLoginPane.Status;
-import static org.omg.IOP.CodecPackage.TypeMismatchHelper.type;
 
 /**
  *
  * @author Nagul
  */
 public class CarPoolingSystem {
-    public double costpermile = 5.2;
-    //public LinkedList<Customer> Customerlist = new LinkedList>Customer>();
-    
+
+    public double costpermile;
+    public LinkedList<Customer> Customerlist = new LinkedList<Customer>();
+
     enum Type {
+
         Driver,
         Passenger
     }
-    
-    
-    public void addCust(Customer c){}
-    
-    public void addRide(Ride r){ }
-    
-    public void searchCust(int customerId, String fName, String lName, String email, String mobile){}
-    
-    public void calcCost(Date sDate, Date eDate, String origin, String dest){}
-    
-    public void getAvailableRide(Date sDate, Date eDate, String origin, String dest){}
-    
-    public void validateCust(int customerId, Type type){}
-    
-    public void searchRide(Customer c){}
-    
-    public void printCustByName(Type t){}
-    
-    public void printRideByOrigin(Status s){}
-    
+
+    public CarPoolingSystem() {
+        costpermile = 0;
+    }
+
+    public CarPoolingSystem(double costpermile) {
+        this.costpermile = costpermile;
+    }
+
+    public void addCustomer(Customer customer) {
+        this.Customerlist.add(customer);
+    }
+
+    public void addRide(Ride r) {
+    }
+
+    public LinkedList<Customer> searchCustomer(long customerId, String fName, String lName, String email, String mobile) {
+        LinkedList<Customer> returnList = new LinkedList<Customer>();
+        for (Customer customer : Customerlist) {
+            if ((customerId > 0) && (customer.getCustomerId() == customerId)) {
+                returnList.add(customer);
+                break;
+            }
+            if ((email != null) && (email.compareToIgnoreCase(customer.getEmail()) == 0)) {
+                returnList.add(customer);
+                break;
+            }
+            if ((mobile != null) && (mobile.compareToIgnoreCase(customer.getMobile()) == 0)) {
+                returnList.add(customer);
+                break;
+            }
+            if ((fName != null) && (fName.compareToIgnoreCase(customer.getfName()) == 0)) {
+                returnList.add(customer);
+                continue;
+            }
+            if ((lName != null) && (lName.compareToIgnoreCase(customer.getlName()) == 0)) {
+                returnList.add(customer);
+                continue;
+            }
+        }
+        return returnList;
+    }
+
+    public void calcCost(Date sDate, Date eDate, String origin, String dest) {
+    }
+
+    public void getAvailableRide(Date sDate, Date eDate, String origin, String dest) {
+    }
+
+    public void validateCust(long customerId, Type type) {
+    }
+
+    public void searchRide(Customer c) {
+    }
+
+    public void printCustByName(Type t) {
+    }
+
+    public void printRideByOrigin(Status s) {
+    }
+
 }
