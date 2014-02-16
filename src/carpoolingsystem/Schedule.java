61797921545ca418fdc0eafe86119ee2dfebf719
AnnamalaiAnna <annamalai1.a@gmail.com>
2014-02-16 16:54:10 -0500

    Day 1 Changes made by Annamalai, Rajesh

--- src/carpoolingsystem/Schedule.java
+++ src/carpoolingsystem/Schedule.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package carpoolingsystem;
+
 import java.util.Date;
 
 /**
@@ -12,47 +12,57 @@ import java.util.Date;
  * @author Radhika
  */
 public class Schedule {
-private float cost;
-private Date startDate;
-private Date endDate;
-private boolean schedStatus;
-private Passenger pass;
-private Ride ride;
 
-public Schedule(){
-    
-}
-public Schedule(Date gstartDate, Date gendDate, Passenger gpass, Ride gride){
-    startDate=gstartDate;
-    endDate=gendDate;
-    pass=gpass;
-    ride=gride;
-}
-public float getCost(){
-    //cost= cost/mile*no of days* no of miles per day
-    return cost;
-}
-public Date getStartDate(){
-    return startDate;
-}
-public Date getEndDate(){
-    return endDate;
-}
-public void deleteSchedule(){
-schedStatus=false;
-    //
-}
-public void modifySchedule(Date givenStartDate, Date givenEndDate){
- //check if different
- //deleteSchedule()
- //Schedule(parameters)  
-}
+    private float cost;
+    private Date startDate;
+    private Date endDate;
+    private boolean status;
+    private Passenger passenger;
+    private Ride ride;
 
-public void printReciept(){
- //getcost
-    //getenddate
-    //getstartdate
-    //getcustomerName
-    //display reciept
-}
+    public Schedule() {
+
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public Schedule(Date gstartDate, Date gendDate, Passenger gpass, Ride gride) {
+        startDate = gstartDate;
+        endDate = gendDate;
+        passenger = gpass;
+        ride = gride;
+    }
+
+    public float getCost() {
+        //cost= cost/mile*no of days* no of miles per day
+        return cost;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void deactivateSchedule() {
+        status = false;
+    }
+
+    public void modifySchedule(Date givenStartDate, Date givenEndDate) {
+        //check if different
+        //deleteSchedule()
+        //Schedule(parameters)  
+    }
+
+    public void printReciept() {
+        //getcost
+        //getenddate
+        //getstartdate
+        //getcustomerName
+        //display reciept
+    }
 }
